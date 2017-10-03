c124c67ede0fb2a0f34e9bc567e32326d954a8b9
Firas Chaaben <firaschaabencss@gmail.com>
2017-10-03 14:10:03 +0100

    fix password input

--- follow/forms.py
+++ follow/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class accesForm(forms.Form):
     username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': "form-control", 'placeholder': 'username'}),label='')
-    password = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': "form-control", 'placeholder': 'password'}),label='')
+    password = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'class': "form-control", 'placeholder': 'password'}),label='')
