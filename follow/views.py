e3e3f3d0cba04bbc29701a6e1db6a4439826a53f
Firas Chaaben <firaschaabencss@gmail.com>
2017-12-18 11:43:28 +0100

    format code

--- follow/views.py
+++ follow/views.py
@@ -15,38 +15,25 @@ browser = webdriver.PhantomJS("/home/firas/git/scripts-python/phantomjs-2.1.1-li
 browser.set_window_size(1120, 550)
 
 def auto_cheek(name,password):
-        """ login cheek
-        if succes return acces valider
-        else return login error from instagram web site
-        """
-
-	browser.get('http://instagram.com')
-
-	time.sleep(10)
-
-	browser.find_element_by_xpath("//a[contains(@class, '_b93kq')]").click()
-
-
-	user_name = browser.find_element_by_name('username')
-	user_name.clear()
-	user_name.send_keys(name)
-
-	password_el = browser.find_element_by_name('password')
-	password_el.clear()
-	password_el.send_keys(password)
-
-	password_el.send_keys(Keys.RETURN)
-
-	time.sleep(10)
-	try:
-		error = browser.find_element_by_id('slfErrorAlert')
-	except NoSuchElementException:
-		error = None
-	if error != None:
-		return error.text
-	else:
-		return 'acces valider'
-
+        browser.get('http://instagram.com')
+        time.sleep(10)
+        browser.find_element_by_xpath("//a[contains(@class, '_b93kq')]").click()
+        user_name = browser.find_element_by_name('username')
+        user_name.clear()
+        user_name.send_keys(name)
+        password_el = browser.find_element_by_name('password')
+        password_el.clear()
+        password_el.send_keys(password)
+        password_el.send_keys(Keys.RETURN)
+        time.sleep(10)
+        try:
+                error = browser.find_element_by_id('slfErrorAlert')
+        except NoSuchElementException:
+                error = None
+        if error != None:
+                return error.text
+        else:
+                return 'acces valider'
 
 def get_detail():
 	time.sleep(5)
